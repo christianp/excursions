271b7dab003d8732b90dba85d516d732eb96169a
Christian Perfect <christianperfect@gmail.com>
2013-07-01 11:24:14 +0100

    filter out licence plates with I and Q

--- cargame/cargame.py
+++ cargame/cargame.py
@@ -4,21 +4,28 @@
 import re
 from operator import itemgetter
 
-words = open('words.txt').read().split('\n')[:-1]
+#read in words
+words = open('../../words.txt').read().split('\n')[:-1]
 
-words = [w for w in words if "'" not in w]
-
+# regex matches any three letters, with any numbers of vowels interspersed
 r=re.compile(r'^[aeiou]*[a-z][aeiou]*[a-z][aeiou]*[a-z][aeiou]*$')
 
+# find words which match the regex
 good_words = [w for w in words if r.match(w)]
 
+# strip out the vowels
 shorts = [(w,re.sub('[aeiou]','',w)) for w in good_words]
 
+# I and Q are not allowed in the last three letters of a licence
+shorts = [w for w in shorts if 'i' not in w and 'q' not in w]
+
+# collect together words which come from the same three letters
 plate_dict={}
 for long,short in shorts:
 	l=plate_dict.setdefault(short,[])
 	l.append(long)
 
+# output
 print('%i endings can be made into words' % len(plate_dict.keys()))
 print('%i words can be made from endings' % len(good_words))
 
